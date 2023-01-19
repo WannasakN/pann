6de2e9098afd6b67ffdd72aa223f3a812f4eb555
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2023-01-19 23:49:59 +0700

    เพิ่ม auth ใน api/index

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,10 +1,11 @@
 import Router from 'koa-router'
 import announ from './announcement'
 import userResult from './user_result'
+import { authMiddleware } from 'auth'
 
 const apiRouter = new Router()
 
-apiRouter.use('/api/announ',announ.routes())
-apiRouter.use('/api/userResult',userResult.routes())
+apiRouter.use('/api/announ',authMiddleware,announ.routes())
+apiRouter.use('/api/userResult',authMiddleware,userResult.routes())
 
 export default apiRouter
