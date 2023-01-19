42831f6480e5109b2f36effeebfb925582352dc2
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2023-01-20 00:12:45 +0700

    แก้ไข index ให้ถูกต้อง

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,13 +1,23 @@
 import Koa from 'koa'
-import json from 'koa-json'
-import apiRouter from './api'
 import { koaBody } from 'koa-body'
+import cors from '@koa/cors'
+
+import loadFixtures from './fixture'
+import apiRouter from './api'
+import appConfig from './config'
+import { initSsoCert } from './auth'
 
 const app = new Koa()
 
-app.use(json())
-app.use(koaBody())
-app.use(apiRouter.routes())
+app.use(cors());
+app.use(koaBody());
 
-app.listen(8000)
+app.use(apiRouter.routes());
+
+(async () => {
+  await loadFixtures(appConfig.ClearDataBeforeLoad)
+  await initSsoCert()
+  app.listen(8000)
+  console.log('Server is ready at port 8000')
+})();
 
