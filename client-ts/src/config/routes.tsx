56e61c1644dec2043b5e39a19313c4795efc0cf6
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2023-01-21 16:49:47 +0700

    เพิ่มหน้าhomeในroutesและให้แสดงUserResultList ในhome

--- client-ts/src/config/routes.tsx
+++ client-ts/src/config/routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/login";
+import UserResultList from "../pages/user-result-list";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route index element={<Login />} />
             <Route path="login" element={<Login />}/>
+            <Route path="home" element={<UserResultList />}/>
         </Routes>
     )
 }
